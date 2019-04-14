7b1796b82a8a1c42a66989354bcd77a606bd6edf
heidygerbi <heidyjoanagr@gmail.com>
2019-04-13 20:21:00 -0500

    eliminar primer campo vacio con un filter y asignación a list (otro array)

--- burger/src/app/orders/order-list/order-list.component.ts
+++ burger/src/app/orders/order-list/order-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderOfflineService } from 'src/app/shared/order-offline.service';
-// import { OrderList } from 'src/app/shared/order.model';
 
 @Component({
   selector: 'app-order-list',
@@ -8,36 +7,16 @@ import { OrderOfflineService } from 'src/app/shared/order-offline.service';
   styleUrls: ['./order-list.component.css']
 })
 export class OrderListComponent implements OnInit {
-  objTotalOrders = {};
   arrObjTotalOrders = [];
-  // list: OrderList;
+  list = [];
   constructor(private data: OrderOfflineService) {
-  // console.log('este es el hermano de recibe (constructor): '+
-  this.data.totalOrder.subscribe(obj => {
-    // this.objTotalOrders = obj;
-    this.arrObjTotalOrders.push(obj)
-  });
-
-  // subscribe(arrObjTotalOrders => this.arrObjTotalOrders));
-    //this.arrObjTotalOrders = arrObjTotalOrders); 
-    // this.arrObjTotalOrder as OrderList;
-    console.log('esto es objTotalOrders: '+this.objTotalOrders);
-  }
- 
-  ngOnInit() {
-    // console.log('este es el hermano de recibe (ngOnInit): '+this.data.totalOrder.subscribe(arrObjTotalOrders => this.arrObjTotalOrders = arrObjTotalOrders));
-
-    // console.log('esto es array: '+this.arrObjTotalOrder); 
-    //  return this.arrObjTotalOrder as OrderList;
+    this.data.totalOrder.subscribe(obj =>{
+       this.arrObjTotalOrders.push(obj);
+       this.list = this.arrObjTotalOrders.filter(item => item.item !== undefined);
+     });
      
-    // console.log('aqui: '+this.objTotalOrder)// as OrderList;  
-    // this.data.currentTotalOrder.subscribe(objTotalOrder => console.log(this.objTotalOrder = objTotalOrder))
-  
+     console.log(this.list);
+    }
+   ngOnInit() {
   }
-
-  // newTotalOrder() {
-  //   this.data.addTotalOrder(this.data.addTotalOrder(this.list.filter(item => item.id=== selectItem))
-  //   })
-  // }
-
 }
